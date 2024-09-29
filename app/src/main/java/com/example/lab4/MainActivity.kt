075288a9901daecbbb3a804af31bb6557d63ffb1
revqueenge <Dealul02@gmail.com>
2024-09-29 16:13:33 +0700

    Добавление кнопки для подсказок, присвоение ей свойств скрытия и раскрытия в разных ситуациях и вывод подсказок в новых toast.

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab4
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var truebutton: Button
     private lateinit var falsebutton: Button
     private lateinit var nextButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
     private val questionBank = listOf(
@@ -42,6 +44,7 @@ class MainActivity : AppCompatActivity() {
         falsebutton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.text_view)
+        cheatButton = findViewById(R.id.cheat_button)
 
         truebutton.setOnClickListener { checkAnswer(true) }
         falsebutton.setOnClickListener { checkAnswer(false) }
@@ -51,8 +54,11 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
             truebutton.visibility = View.VISIBLE
             falsebutton.visibility = View.VISIBLE
+            cheatButton.visibility = View.VISIBLE
             nextButton.visibility = View.INVISIBLE // Сбросить видимость для следующего вопроса
         }
+
+        cheatButton.setOnClickListener { showHint() }
 
         updateQuestion()
 
@@ -78,6 +84,7 @@ class MainActivity : AppCompatActivity() {
 
         truebutton.visibility = View.INVISIBLE
         falsebutton.visibility = View.INVISIBLE
+        cheatButton.visibility = View.INVISIBLE
 
         if (currentIndex < questionBank.size - 1) {
             nextButton.visibility = View.VISIBLE
@@ -94,6 +101,23 @@ class MainActivity : AppCompatActivity() {
         // Сбросить видимость кнопок для нового вопроса.
         truebutton.visibility = View.VISIBLE
         falsebutton.visibility = View.VISIBLE
+    }
+
+    private fun showHint() {
+        val hintMessage = when (currentIndex) {
+            0 -> "Бумагу и правда делают из дерева"
+            1 -> "Тихий океан - 178млн км... Атлантический поменьше..."
+            2 -> "Радуга - преломление солнечных лучей"
+            3 -> "Стыдно не знать, но да, это так"
+            4 -> "Тут ошибочка на 100 лет"
+            5 -> "четное + четное..."
+
+            else -> "Нет подсказки для этого вопроса"
+        }
+
+        val toast = Toast.makeText(this, hintMessage, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 100)
+        toast.show()
     }
 
     private fun showFinalScore() {
