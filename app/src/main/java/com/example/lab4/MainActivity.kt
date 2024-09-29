389643e845a4d9942384fd2e9b8e9487265fcb26
revqueenge <Dealul02@gmail.com>
2024-09-29 13:17:52 +0700

    Обработка переключения вопросов и добавление новых вопросов.

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.lab4
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var truebutton: Button
     private lateinit var falsebutton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_paper, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_rainbow, true),
+        Question(R.string.question_moscow, true),
+        Question(R.string.question_icebattle, false),
+        Question(R.string.question_math, false))
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -24,6 +38,32 @@ class MainActivity : AppCompatActivity() {
         }
         truebutton = findViewById(R.id.true_button)
         falsebutton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.text_view)
+
+        truebutton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+        falsebutton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
+        }
+
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
 
 
     }
