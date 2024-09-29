3012e9d52b48fecda6d1ba062013f2237d943219
revqueenge <Dealul02@gmail.com>
2024-09-29 12:22:54 +0700

    Добавление и иницилизация компонентов

--- app/src/main/java/com/example/lab4/MainActivity.kt
+++ app/src/main/java/com/example/lab4/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.lab4
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var truebutton: Button
+    private lateinit var falsebutton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +22,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        truebutton = findViewById(R.id.true_button)
+        falsebutton = findViewById(R.id.false_button)
+
+
     }
 }
